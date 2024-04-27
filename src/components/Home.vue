8963f3f0591ed9bd66761aafb670d941867270a6
sindi221 <164901389+sindi221@users.noreply.github.com>
2024-04-27 19:07:05 +0700

    Add files via upload

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <div class="wrapper">
+        <img src="/src/assets/PAGE.SINDY-removebg-preview.png" alt="">
+    </div>
+
+    <text class="about">Halo teman, nama saya Sindi. Saya berasal dari Tapung Hilir. 😘</text>
+    </header>
+    </template>
+    
+<style>
+.about {
+  color: aliceblue;
+}
+</style>
